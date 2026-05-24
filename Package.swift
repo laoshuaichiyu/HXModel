90dd8213e31942c44ad5e1c4876ee73a8f5c65b5
huaixu <840132742@qq.com>
2026-05-24 13:25:40 +0800

    1.test

--- Package.swift
+++ Package.swift
@@ -1,29 +1,27 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "HXModel",
     platforms: [
-        .iOS("16.0"),
-        .macOS("13.0"),
-        .tvOS("16.0"),
-        .watchOS("9.0")
+        .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HXModel",
-            targets: ["HXModel"]),
+            targets: ["HXModel"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HXModel", path: "Sources"),
+            name: "HXModel",
+            path: "HXModel"
+        ),
         .testTarget(
             name: "HXModelTests",
-            dependencies: ["HXModel"]),
+            dependencies: ["HXModel"],
+            path: "Tests/HXModelTests"
+        )
     ]
 )
--- Tests/HXModelTests/HXModelTests.swift
+++ Tests/HXModelTests/HXModelTests.swift
@@ -2,11 +2,9 @@ import XCTest
 @testable import HXModel
 
 final class HXModelTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testWorksInit() {
+        let works = Works()
+        XCTAssertEqual(works.id, 0)
+        XCTAssertEqual(works.name, "世界树")
     }
 }
