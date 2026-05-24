82355da08260f1ea24af8d703d50b638c041c1c8
huaixu <840132742@qq.com>
2026-05-24 11:23:19 +0800

    1.test

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HXModel"),
+            name: "HXModel", path: "Sources"),
         .testTarget(
             name: "HXModelTests",
             dependencies: ["HXModel"]),
